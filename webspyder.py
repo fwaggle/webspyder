780dce2208efdb04a10fe8910a57695d66f0d09c
fwaggle <fwaggle@fwaggle.org>
2014-04-10 10:22:50 +1000

    It crawls!

--- webspyder.py
+++ webspyder.py
@@ -2,11 +2,27 @@
 
 import argparse
 import urllib2
+import urlparse
+import re
+from HTMLParser import HTMLParser
 
 results = {}
 depth = 0
+maxdepth = 0
+domains = {}
 
-class crawlResult(object):
+class SpyderHTMLParser(HTMLParser):
+    links = []
+    
+    def handle_starttag(self, tag, attr):
+        if tag != 'a':
+            return
+
+        for n in attr:
+            if n[0] == 'href':
+                self.links.append(n[1])
+
+class SpyderCrawlResult(object):
     url = ''
     result = 0
     
@@ -19,22 +35,56 @@ def log(res, url, referer):
     print "\t%s" % referer
 
 def crawl(url, referer=None):
-    global results, depth
+    global results, depth, domains
+
+    u = urlparse.urlparse(url)
+
+    # Create absolute URLs
+    if u.netloc == '':
+        ref = urlparse.urlparse(referer)
+        url = urlparse.urljoin(referer, url)
+        u = urlparse.urlparse(url)
+
+    # Only hit allowable domains
+    if u.netloc not in domains:
+        print "Foreign link: %s, aborting this leaf." % url
+        return
+    else:
+        if domains[u.netloc] == False:
+            print "Avoiding link: %s" % url
+            return
 
     # Don't hit the same page twice, but log the result anyway
     if url in results:
         res = results[url]
         log(res.result, url, referer)
         return
+
+    # Don't recurse deeper than maxdepth
+    if depth > maxdepth:
+        return
+    
+    depth = depth + 1 
     
     try:
         req = urllib2.urlopen(url)
         code = req.code
     except urllib2.HTTPError as e:
         code = e.code    
+    except:
+        code = 999
+    
+    if code == 200:
+        parser = SpyderHTMLParser()
+        parser.feed(req.read())
+        
+        for l in parser.links:
+            crawl(l, url)
+    
+    depth = depth - 1
     
     # Stuff the result into our results list and log
-    results[url] = crawlResult(url, code)
+    results[url] = SpyderCrawlResult(url, code)
     log(code, url, referer)
 
 if __name__ == '__main__':
@@ -43,8 +93,13 @@ if __name__ == '__main__':
     parser.add_argument('--depth', '-d', metavar='N', type=int, default=5, help='Maximum depth to traverse from starting URL.')
     
     args = parser.parse_args()
+
+    maxdepth = args.depth
     
     for url in args.url:
-        crawl(url)
+        u = urlparse.urlparse(url)
+        if u.netloc not in domains or domains[u.netloc] != False:
+            domains[u.netloc] = True
+            crawl(url)
     
     print "Finished."
